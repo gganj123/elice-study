9fe96b7ec63ccb658c6fbcb24de023f47b1ff5f2
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-08 02:09:17 +0900

    rest API , mongoose , mongoDB study

--- elice0207/test/app.js
+++ elice0207/test/app.js
@@ -1,19 +1,28 @@
-// app.js
-
 const express = require('express');
+const mongoose = require("mongoose");
+// const productsRouter = require('./products');
+// const cartsRouter = require('./carts');
+const Movie = require('./models/Movie')
 const app = express();
-const productsRouter = require('./products');
-const cartsRouter = require('./carts');
 
+mongoose.connect('mongodb+srv://gganj123:zxcv0903@cluster0.cdypmi2.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> console.log('Connected!'));
+
+
+app.get('/', async (req,res)=>{
+
+    const value = await Movie.find({})
+    res.json(value);
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/',(res,req)=>{
-    res.send('welcome app');
-})
+// app.get('/',(res,req)=>{
+//     res.send('welcome app');
+// })
 
-app.use('/products',productsRouter);
-app.use('/carts',cartsRouter)
+// app.use('/products',productsRouter);
+// app.use('/carts',cartsRouter)
