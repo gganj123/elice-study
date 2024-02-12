4e43345df328b8f087ced27467cbd7b0f1a5cead
gganj123 <zwol10@naver.com>
2024-02-12 21:35:13 +0900

    test: blog practice

--- elice0208/test/app.js
+++ elice0208/test/app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 mongoose.connect('mongodb+srv://gganj123:zxcv0903@cluster0.cdypmi2.mongodb.net/?retryWrites=true&w=majority')
-.then(()=> console.log('Connected!'));
+.then(()=> console.log('podt man'));
 
 
 const PORT = 3000;
